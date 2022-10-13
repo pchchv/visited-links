eb84dad8582f33ff6ea4b1808d6a358003491ffd
pchchv <ipchchv@gmail.com>
2022-10-13 20:59:22 +0300

    Implement a POST method

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
+from typing import List
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    links: List[str]
 
 
 @app.get("/")
 async def root():
     return {"message": "Visited links server. Version 0.0.1"}
+
+@app.post("/visited_links")
+async def create_item(item: Item):
+    # TODO: Add timestamps
+    # TODO: Implement data loading into the database
+    return {"status": "ok"}
