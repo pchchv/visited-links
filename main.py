9b29493061fc9981c2333dff14ff989a291a4921
pchchv <ipchchv@gmail.com>
2022-10-14 08:30:34 +0300

    Implement mongo client and enviroment file

--- main.py
+++ main.py
@@ -1,19 +1,25 @@
+import os
 from typing import List
 from fastapi import FastAPI
+from dotenv import load_dotenv
 from pydantic import BaseModel
+from pymongo import MongoClient
+
 
 app = FastAPI()
+load_dotenv('.env')
+client = MongoClient(os.getenv('MONGO'))
 
 
 class Item(BaseModel):
     links: List[str]
 
 
-@app.get("/")
+@app.get('/')
 async def root():
-    return {"message": "Visited links server. Version 0.0.1"}
+    return {'message': 'Visited links server. Version 0.0.1'}
 
-@app.post("/visited_links")
+@app.post('/visited_links')
 async def create_item(item: Item):
     # TODO: Add timestamps
     # TODO: Implement data loading into the database
